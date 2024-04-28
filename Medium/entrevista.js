2e5005b1725481854f86465a241d700ea16a5d8f
Igor Grieder Melo <igorgrieder@gmail.com>
2024-04-28 08:04:56 -0300

    Refresh

--- Medium/entrevista.js
+++ Medium/entrevista.js
@@ -1,9 +1,3 @@
-// CONSIDERACOES INICIAIS ----------------------------------------------------------------------------------------
-// cada peca possui duas letras e um numero no meio
-// uma peca encaixa na outra quandos eu numero esquerdo for igual ao numero direito da outra peca
-// o numero esquerdo da primeira peca eh sempre zero!
-// o numero direito da ultima peca eh sempre 1!
-// numeros nao se repetem em direita e esquerda
 // criar um programa que recebe a matriz de pecas e retorne a palavra escrita
 // eh necessario montar o quebra cabecas e retornar a string que forma
 // o numero inicial tem na esquerda 0, o final tem na direita 1!
@@ -32,6 +26,35 @@ const quebraCabeca = (nums, tamanho) => {
   return answ
 }
 
+const QuebraCabeca = (nums, tamanho) => {
+  let answ = ''
+  let next
+  const controlLeft = {}
+  const controlRight = {}
+
+  // encontrar a primeira peca e mapear
+  for (let i = 0; i < tamanho; i++) {
+    if (controlLeft[nums[i][0]] === undefined) {
+      controlLeft[nums[i][0]] = nums[i][1]
+    }
+    if (controlRight[nums[i][1]] === undefined) {
+      controlRight[nums[i][1]] = nums[i][2]
+    }
+
+    if (nums[i][0] === 0) {
+      answ += nums[i][1]
+      next = nums[i][2]
+    }
+  }
+
+  for (let k = 0; k < tamanho - 1; k++) {
+    answ += `${controlLeft[next]}`
+    next = controlRight[answ[answ.length - 1]]
+  }
+
+  return answ
+}
+
 const quebra = [[0, 'I', 4], [10, 'r', 1], [4, 'g', 6], [6, 'o', 10]]
 const size = 4
-console.log(quebraCabeca(quebra, size))
+console.log(QuebraCabeca(quebra, size))
